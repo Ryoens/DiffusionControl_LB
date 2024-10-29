9e943b7a52de6eac3869437526b90353c118b84a
enkita <mj66004@e.hiroshima-cu.ac.jp>
2024-10-29 23:19:29 +0900

    実験複数化に対応

--- cmd/Execute.sh
+++ cmd/Execute.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 count=0
+attempt_count=1
 
 # 各パラメータの入力
 read -p "feedback: " feedback
@@ -10,41 +11,68 @@ echo $feedback $threshold $kappa
 
 echo "-------- parameter OK --------"
 
+read -p "仮想ユーザ数: " vus
+read -p "number of attempts: " attempt
+echo $attempt
+
+# timestampディレクトリを作成
+timestamp_data=$(date +"%Y%m%d_%H%M%S")
+data_dir="../../data/${timestamp_data}"
+mkdir -p "$data_dir"
+
 # クラスタ数をコンテナ数から取得
 container=$(docker ps --filter "name=_LB" --format "{{.Names}}" | head -n 1)
 KEY=${container:7:1}
-
 echo "number of clusters: " $KEY
 
-# go run $file -t $feedback -q $threshold -k $kappa
-# プログラムの実行 (gRPCが起動しない場合の挙動も必要) -> 仮想ブリッジの問題
-while [ $count -le $KEY ]
+while [ $attempt_count -le $attempt ]
 do
-    docker exec -d Cluster${count}_LB go run mirror.go -t $feedback -q $threshold -k $kappa &
-    docker exec Cluster${count}_LB ps aux # goのプロセスが走っていなかったらやり直しにしたい
-    count=`expr $count + 1`
-done
+    echo "-------- $attempt_count --------"
+    count=0
 
-# 実験データの取得
-read -p "仮想ユーザ数: " vus
-echo $vus
+    # go run $file -t $feedback -q $threshold -k $kappa
+    # プログラムの実行 (gRPCが起動しない場合の挙動も必要) -> 仮想ブリッジの問題
+    while [ $count -le $KEY ]
+    do
+        docker exec -d Cluster${count}_LB go run mirror.go -t $feedback -q $threshold -k $kappa /bin/bash
+        docker exec Cluster${count}_LB ps aux # goのプロセスが走っていなかったらやり直しにしたい
+        count=`expr $count + 1`
+    done
 
-## k6による負荷テスト
-timestamp=$(date +"%Y%m%d_%H%M%S")
-mpstat -P 1-9 1 60 | awk -v OFS=',' \
-'BEGIN {print "Timestamp","CPU","%user","%nice","%system","%iowait","%irq","%soft","%steal","%idle"} 
-NR>4 {print strftime("%H:%M:%S"), $3, $4, $5, $6, $7, $8, $9, $10, $NF}' > ../../data/cpu_usage_${timestamp}.csv &
-k6 run ../test.js --vus $vus --summary-export=../../data/summary"_"$timestamp.json
-# --------------------------
+    # 実験データの取得
+    sleep 1
+    echo $vus
 
-## curlで大量にリクエストを送信
-# test.shを実行したい
-
-for num in $(seq 2 $((count + 1)))
-do 
-    i=$((num - 2)) 
-    echo "Cluster$i" 
+    ## k6による負荷テスト
     timestamp=$(date +"%Y%m%d_%H%M%S")
-    # 現在のディレクトリとは離れたところにデータを残す
-    curl -X GET 114.51.4.$num:8002 -o ../../data/Cluster$i"_"$timestamp.csv
+    mpstat -P 0-9 1 60 | awk -v OFS=',' \
+    'BEGIN {print "Timestamp","CPU","%user","%nice","%system","%iowait","%irq","%soft","%steal","%idle"} 
+    NR>4 {print strftime("%H:%M:%S"), $3, $4, $5, $6, $7, $8, $9, $10, $NF}' > "${data_dir}/cpu_usage_${timestamp}.csv" &
+    k6 run ../test.js --vus $vus --summary-export="${data_dir}/summary"_"$timestamp.json"
+    # --------------------------
+
+    ## curlで大量にリクエストを送信
+    # test.shを実行したい
+
+    for num in $(seq 2 $((count + 1)))
+    do 
+        i=$((num - 2)) 
+        echo "Cluster$i" 
+        timestamp=$(date +"%Y%m%d_%H%M%S")
+        # 現在のディレクトリとは離れたところにデータを残す
+        curl -X GET 114.51.4.$num:8002 -o "${data_dir}/Cluster$i"_"$timestamp.csv"
+    done
+
+    attempt_count=`expr $attempt_count + 1`
+    sleep 10
 done
+
+# パラメータなどをファイルに書き出し
+timestamp=$(date +"%Y%m%d_%H%M%S")
+{
+echo "Experiment in these parameters is finished"
+echo "feedback: $feedback [ms]"
+echo "threshold: $threshold"
+echo "kappa: $kappa"
+echo "virtual users: $vus [users]"
+} > "${data_dir}/parameters"_"$timestamp"
